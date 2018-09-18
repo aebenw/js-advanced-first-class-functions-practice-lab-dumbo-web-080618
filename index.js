b3f8e75e6c05c2f69aab21f15839bced7222e1c7
github username <github email address>
2018-09-17 21:06:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Code your solution in this file!
+// function logDriverNames(arr) {
+//   const copy = arr.filter(function(driver){
+//     console.log(driver.name)
+//   });
+//   return copy
+// };
+// const reading = function (arr) {
+//   arr.forEach(function(item){
+//     console.log(driver[`${item}`])
+//   });
+// };
+//
+// const logDriverNames = reading(arr)
+//
+// logDriverNames(name)
+
+function logDriverNames(arr) {
+  arr.forEach(function(driver){
+    console.log(driver.name)
+  });
+};
+
+function logDriversByHometown(arr, home){
+  const newArr = arr.filter(function(driver){
+    return driver.hometown === home
+  });
+  logDriverNames(newArr)
+}
+
+
+function compare(a,b) {
+  let nameA = a.name.toUpperCase();
+  let nameB = b.name.toUpperCase();
+  if (nameA < nameB)
+    return -1;
+  if (nameA > nameB)
+    return 1;
+  return 0;
+};
+
+function driversByRevenue(arr) {
+  let copy = Object.create(arr)
+  return copy.sort(function(a,b) {
+  return a.revenue - b.revenue
+});
+};
+
+function driversByName(arr) {
+  let copy = Object.create(arr)
+  return copy.sort(compare)
+};
+
+function totalRevenue(driver) {
+  return driver.reduce(function (acc, cur) {
+    return acc + cur.revenue
+  }, 0);
+
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers)/drivers.length
+};
